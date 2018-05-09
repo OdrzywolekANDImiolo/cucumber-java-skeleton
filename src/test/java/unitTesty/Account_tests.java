ae6223f9e560bc0aafa73f8d5d8d3e8a1a6fdf21
Jozef <jozef.imiolo@gmail.com>
2018-05-09 21:12:12 +0200

    Add unitests class Account and modify unittests class Client

---
+++ src/test/java/unitTesty/Account_tests.java
@@ -0,0 +1,50 @@
+package unitTesty;
+
+import cucumber.api.java.en_old.Ac;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import progob.Account;
+import progob.Client;
+import progob.WithdrawException;
+
+import static junit.framework.TestCase.fail;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+
+
+public class Account_tests {
+    private Account konto;
+    @Before
+    public void setup() {
+        Client klient = new Client("Kamil", "Nowak");
+        konto = new Account(klient);
+    }
+    @Test
+    public void testEqualsNullIsFalse() {
+        assertFalse(konto.equals(null));
+    }
+    @Test
+    public void testInitbalance() {
+        assertEquals(0, konto.getBalance(),0);
+    }
+    @Test
+    public void testAddBalance() {
+        konto.addBalance(20);
+        assertEquals(20.0, konto.getBalance(),0);
+    }
+    @Test
+    public void testWithdraw() throws WithdrawException {
+        konto.addBalance(20);
+
+        try {
+            konto.withdraw(10);
+            assertEquals(10, konto.getBalance(), 0);
+            konto.withdraw(20);
+            fail();
+        }
+        catch (WithdrawException e)
+        {}
+    }
+
+}
--- src/test/java/unitTesty/Client_tests.java
+++ src/test/java/unitTesty/Client_tests.java
@@ -5,12 +5,18 @@ import org.junit.Before;
 import org.junit.Test;
 import progob.Client;
 
+import static org.junit.Assert.assertFalse;
+
 public class Client_tests {
 
     private Client klient;
     @Before
     public void setup() {
         klient = new Client("Kamil", "Nowak");
+    }
+    @Test
+    public void testEqualsNullIsFalse() {
+        assertFalse(klient.equals(null));
     }
     @Test
     public void testFirstName() {
